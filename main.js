6b468c3297e540d2befe8cae9a790874905408f4
gloria bello <calderonscarlette69@gmail.com>
2022-09-27 13:25:05 -0300

    ciclos

---
+++ main.js
@@ -0,0 +1,20 @@
+let n=1
+do{
+    console.log(n);
+    n= n+1 
+}while(n<=10
+    )
+
+let n2=10 
+do{
+    console.log(n2);
+    n2=n2 - 1
+}while(n2>=1) 
+
+const name="gloria"
+n=name.length -1
+do {
+    console.log(name[n]) 
+    n--
+
+}while (n>=0)
